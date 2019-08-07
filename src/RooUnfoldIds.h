47c6d18b4e2bc11c74dfe7ee7243e6b23a9c926c
Pim Jordi Verschuuren <pverschu@lxplus745.cern.ch>
2019-08-07 15:24:55 +0200

    Rewrote GetUnfold.. and GetAdet.. functions. The IDS implementation compiles.

--- src/RooUnfoldIds.h
+++ src/RooUnfoldIds.h
@@ -57,8 +57,8 @@ template<class Hist, class Hist2D>
   
   virtual void Reset();
 
-  TH2* GetUnfoldCovMatrix(const TH2 *cov, Int_t ntoys, Int_t seed = 1) const;
-  TH2* GetAdetCovMatrix(Int_t ntoys, Int_t seed = 1) const;
+  TMatrixD* GetUnfoldCovMatrix(const TMatrixD *cov, Int_t ntoys, Int_t seed = 1) const;
+  TMatrixD* GetAdetCovMatrix(Int_t ntoys, Int_t seed = 1) const;
 
  protected:
   void Assign(const RooUnfoldIdsT &rhs); // implementation of assignment operator
